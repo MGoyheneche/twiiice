1c2ff4cfd1297e1101895a8a548c5542853ac955
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2014-02-27 17:34:04 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twiiice::Application.config.secret_key_base = '038c87b2980da94c43d324e2c8c0fa8f993f98bafa501ae05f5d502f55e364adf1a47a3dc8f90f18722e9b47f9dc489e66755600f6e22d1ca5112be0e52b784e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twiiice::Application.config.session_store :cookie_store, key: '_twiiice_session'
